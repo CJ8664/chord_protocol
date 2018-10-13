fd106814bbce596f6bbc4a557ffecc4800ab9cf2
Chirag Jain <csjain@ncsu.edu>
2018-10-12 22:15:11 -0400

    Initial Commit

---
+++ env_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "****** Setting up Environment *******"
+echo "###### Copying binaries #############"
+sudo cp -pf chord.py /usr/bin/chord
+echo "###### Done #########################"
+echo "************ ALL DONE ! *************"
